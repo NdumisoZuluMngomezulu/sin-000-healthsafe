8ea1adf1701304e3307114d4934b1a03988fa110
Ndumiso Zulu <ndzuljhb025@student.wethinkcode.co.za>
2026-08-30 14:46:40 +0200

    feat : exposed endpoints in api server

--- ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
@@ -1,11 +1,28 @@
 package co.wethinkcode.healthsafe;
 
+import co.wethinkcode.healthsafe.service.CSVLoader;
+
+import java.util.List;
+import java.io.IOException;
+
+import co.wethinkcode.healthsafe.model.Ward;
+
 import io.javalin.Javalin;
 
 public class IngestionServiceApp {
 
     public static void main(String[] args) {
+        
+        CSVLoader loader = new CSVLoader();
+        try {
+            List<Ward> wards = loader.getWards();
+        } catch (IOException e) {
+            System.out.println("Could not load wards" + e.getMessage();)
+        }
+        
         Javalin app = Javalin.create().start(7030);
+
+        app.get("/wards", ctx -> {ctx.json(wards);});
 
         app.get("/health", ctx -> ctx.result("OK"));
 
