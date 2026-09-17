71b2331393343af4259aeb6561305785764f1878
Ndumiso Zulu <ndzuljhb025@student.wethinkcode.co.za>
2026-09-17 19:23:22 +0200

    removed unused imports

--- staffing-service/src/main/java/co/wethinkcode/healthsafe/service/StaffingHandler.java
+++ staffing-service/src/main/java/co/wethinkcode/healthsafe/service/StaffingHandler.java
@@ -4,8 +4,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
